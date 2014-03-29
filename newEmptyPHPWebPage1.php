c0c5b9c85ebb07d0bf35fc1cc1b6e8e3714d51b5
User <User@Seila-PC>
2014-03-29 09:56:47 +0700

    new commit

---
+++ newEmptyPHPWebPage1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $string1 = "Hello World\n";
+            if ($string1 =' m/(H..).(o..)/') {
+              print "We matched '$1' and '$2'\n";
+            }
+        ?>
+    </body>
+</html>
